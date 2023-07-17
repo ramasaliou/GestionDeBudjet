82ba3d79ac613186b18230b9cefb6fa030c71b7e
ramasaliou <niamgramatoulaye925@gmail.com>
2023-07-17 13:24:31 +0000

    fix changes

--- index.js
+++ index.js
@@ -1,103 +1,11 @@
 
-<<<<<<< HEAD
-// // var form = document.getElementById('myform');
-
-// // // Ajouter evenement de summission/
-// // form.addEventListener('submit', function(event) {
-// //   event.preventDefault();
-
-// //   // / Recuperer les valeurs du formulaire /
-// //   var titre = document.getElementById('titre').value;
-// //   var montant = document.getElementById('montant').value;
-
-// //   // / Affecter a un object /
-// //   var formData = {
-// //     titre: titre,
-// //     montant: titre
-// //   }
-
-// //   // / Stocker l'object dans localstorage /
-// //   // var depenses = localSotorage.getItem('formData', json.stringify(formData))
-// //    localStorage.setItem('formData', JSON.stringify(formData));
-
-// //   // / Envoyer un message d'alert */
-// //   alert('form submitting succesfully')
-// // })
-
-// // /*Recuperation des donnees du localstorage*/
-// // var table_depense = localStorage.getItem('formData')
-// // console.log('table_depense', table_depense)
-
-// // /* Convertir les donnees */
-// // var data = JSON.parse(table_depense)
-
-=======
-// // Recuperer le formulaire 
-// var form = document.getElementById('myform');
-
-// // Ajouter evenement de summission/
-// form.addEventListener('submit', function(event) {
-//   event.preventDefault();
-
-//   // / Recuperer les valeurs du formulaire /
-//   var titre = document.getElementById('titre').value;
-//   var montant = document.getElementById('montant').value;
-
-//   // / Affecter a un object /
-//   var formData = {
-//     titre: titre,
-//     montant: montant
-//   }
-
-//   // / Stocker l'object dans localstorage /
-//   // var depenses = localSotorage.getItem('formData', json.stringify(formData))
-//    localStorage.setItem('formData', JSON.stringify(formData));
-
-//   // / Envoyer un message d'alert */
-//   alert('form submitting succesfully')
-// })
-
-// /*Recuperation des donnees du localstorage*/
-// var table_depense = localStorage.getItem('formData')
-// console.log('table_depense', table_depense)
-
-// /* Convertir les donnees */
-// var data = JSON.parse(table_depense)
-  
-// //  Creation du tableau 
-//   var tableau = document.createElement('table');
-// //    Creation du corps du tableau 
-//   var tbody = document.createElement('tbody');
-// //   Recuperation des donnees 
-//   for (var key in data) {
-//     // / Creation d'une nouvelle ligne /
-//     var ligne = document.createElement('tr');
-//     // / Creation d'une nouvelle cellule pour le titre/
-//     var celluleTitre = document.createElement('td');
-//       // celluleTitre.textContent = key;
-//     // / Creation d'une nouvelle cellule pour le montant' /
-//     var celluleMontant = document.createElement('td');
-//       celluleMontant.textContent = data[key];
-//     // / Ajout du cellule titre a la nouvelle lignr /
-//       // ligne.appendChild(celluleTitre);
-//     // / Ajout du cellule montant a la nouvelle ligne /
-//     ligne.appendChild(celluleMontant);
-//     // / Ajout de la ligne au corps du tableau /
-//       tbody.appendChild(ligne);
-//   }
-  
-// //    Ajout du corps du tableau au niveau de notre tableau /
-//   tableau.appendChild(tbody);
-// //    Branchement du tableau au niveau du dom */
-//   var containers = document.getElementById('containers');
-//   containers.appendChild(tableau);
 
 
 // Récupération du formulaire
 var form = document.getElementById('myform');
 
 // Ajout d'un événement de soumission pour le formulaire
-form.addEventListener('submit', function(event) {
+  form.addEventListener('submit', function(event) {
   event.preventDefault();
 
   // Récupération des valeurs du formulaire
@@ -116,7 +24,18 @@ form.addEventListener('submit', function(event) {
 
   if (table_depense) {
     data = JSON.parse(table_depense);
+
   }
+  
+
+
+
+  
+
+ 
+
+
+
 
   // Ajout des nouvelles données à la liste existante
   data.push(formData);
@@ -129,50 +48,17 @@ form.addEventListener('submit', function(event) {
 
   // Réinitialisation du formulaire
   form.reset();
->>>>>>> 83ab13d3afab92886f38d3d4608bac0821aa05c4
 
   // Affichage d'un message de confirmation
   alert('Formulaire soumis avec succès');
 });
 
-<<<<<<< HEAD
-  
-// //  Creation du tableau 
-//   var tableau = document.createElement('table');
-// //    Creation du corps du tableau 
-//   var tbody = document.createElement('tbody');
-// //   Recuperation des donnees 
-//   for (var key in data) {
-//     // / Creation d'une nouvelle ligne /
-//     var ligne = document.createElement('tr');
-//     // / Creation d'une nouvelle cellule pour le titre/
-//     var celluleTitre = document.createElement('td');
-//       celluleTitre.textContent = key;
-//     // / Creation d'une nouvelle cellule pour le montant' /
-//     var celluleMontant = document.createElement('td');
-//       celluleMontant.textContent = data[key];
-//     // / Ajout du cellule titre a la nouvelle lignr /
-//       ligne.appendChild(celluleTitre);
-//     // / Ajout du cellule montant a la nouvelle ligne /
-//     ligne.appendChild(celluleMontant);
-//     // / Ajout de la ligne au corps du tableau /
-//       tbody.appendChild(ligne);
-//   }
-  
-// //    Ajout du corps du tableau au niveau de notre tableau /
-//   tableau.appendChild(tbody);
-// //    Branchement du tableau au niveau du dom */
-//   var containers = document.getElementById('containers');
-//   containers.appendChild(tableau);
-// // //   pour le html   Tableau depenses
-// // //     <div id="container"></div>
 
-=======
+
 // Fonction pour afficher le tableau avec les données
 function afficherTableau(data) {
   // Récupération de l'élément conteneur du tableau
   var containers = document.getElementById('containers');
->>>>>>> 83ab13d3afab92886f38d3d4608bac0821aa05c4
 
   // Suppression des anciennes données du tableau
   while (containers.firstChild) {
@@ -197,7 +83,6 @@ function afficherTableau(data) {
 
     var celluleMontant = document.createElement('td');
     celluleMontant.textContent = row.montant;
-
     var celluleActions = document.createElement('td');
     var boutonSupprimer = document.createElement('button');
     boutonSupprimer.textContent = 'Supprimer';
@@ -210,6 +95,7 @@ function afficherTableau(data) {
 
       // Suppression de la ligne du tableau
       ligneParente.parentNode.removeChild(ligneParente);
+
     });
 
     celluleActions.appendChild(boutonSupprimer);
@@ -240,15 +126,133 @@ afficherTableau(data);
 //modal
 let   modal = document.getElementById("ajoutModal")
 let   modalButton = document.getElementById("addDepenseButton")
+
 let close = document.querySelector(".close")
 let openModalDep = document.getElementById("openModalDep")
+
 // openModalDep.addEventListener('click',function() {
 //     modal.style.display = 'block'
 // })
 openModalDep.onclick = function(){
     modal.style.display='block'
 }
-
 close.onclick = function(){
-    modal.style.display='none'
+  modal.style.display='none'
 }
+
+//modal2
+let   modal2 = document.getElementById("ajoutModal2")
+let   modalButton2 = document.getElementById("addRevenuButton")
+
+let clos = document.querySelector(".clos")
+let openModalRev = document.getElementById("openModalRev")
+
+// openModalDep.addEventListener('click',function() {
+//     modal.style.display = 'block'
+// })
+openModalRev.onclick = function(){
+    modal2.style.display='block'
+}
+
+
+
+clos.onclick = function(){
+  modal2.style.display='none'
+}
+//partie revenu
+
+// Récupération du formulaire
+var form1 = document.getElementById('myform1');
+
+// Ajout d'un événement de soumission pour le formulaire
+form1.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+// Récupération des valeurs du formulaire
+  var titre2 = document.getElementById('titre2').value;
+  var montant2 = document.getElementById('montant2').value;
+
+ // Création d'un objet avec les données du formulaire
+ var formData1 = {
+  titre: titre2,
+  montant: montant2
+};
+ // Récupération des données existantes dans le localStorage
+ var table_depense1 = localStorage.getItem('formData1');
+ var data1 = [];
+ if (table_depense1) {
+  data1 = JSON.parse(table_depense1);
+}
+// Ajout des nouvelles données à la liste existante
+data1.push(formData1);
+
+  // Stockage des données mises à jour dans le localStorage
+  localStorage.setItem('formData1', JSON.stringify(data1));
+
+  // Appel de la fonction pour afficher le tableau avec les données mises à jour
+  afficherTableau1(data1);
+
+   // Réinitialisation du formulaire
+   form1.reset();
+
+     // Affichage d'un message de confirmation
+  alert('Formulaire soumis avec succès');
+});
+// Fonction pour afficher le tableau avec les données
+function afficherTableau1(data1) {
+  // Récupération de l'élément conteneur du tableau
+  var containers1 = document.getElementById('containers1');
+
+   // Suppression des anciennes données du tableau
+   while (containers1.firstChild) {
+    containers1.removeChild(containers1.firstChild);
+  }
+  // Création du tableau
+  var tableau1 = document.createElement('table');
+  tableau1.classList.add('tableau-style');
+  // Création du corps du tableau
+  var tbody = document.createElement('tbody');
+
+  // Parcours des données pour créer les lignes du tableau
+  for (var i = 0; i < data1.length; i++) {
+    var row1 = data1[i];
+    var ligne1 = document.createElement('tr');
+
+    var celluleTitre1 = document.createElement('td');
+    celluleTitre1.textContent = row1.titre;
+
+    var celluleMontant1 = document.createElement('td');
+    celluleMontant1.textContent = row1.montant;
+    var celluleActions1 = document.createElement('td');
+    var boutonSupprimer1 = document.createElement('button');
+    boutonSupprimer1.textContent = 'Supprimer';
+    boutonSupprimer1.classList.add('btn-supprimer');
+
+    // Gestionnaire d'événement pour le clic sur le bouton "Supprimer"
+    boutonSupprimer1.addEventListener('click', function() {
+      // Récupération de la ligne parente (tr) du bouton
+      var ligneParente1 = this.parentNode.parentNode;
+
+      // Suppression de la ligne du tableau
+      ligneParente1.parentNode.removeChild(ligneParente1);
+    });
+    celluleActions1.appendChild(boutonSupprimer1);
+
+    ligne1.appendChild(celluleTitre1);
+    ligne1.appendChild(celluleMontant1);
+    ligne1.appendChild(celluleActions1);
+
+    tbody.appendChild(ligne1);
+  }
+  tableau1.appendChild(tbody);
+  containers1.appendChild(tableau1);
+}
+// Récupération des données existantes dans le localStorage et affichage initial du tableau
+var table_depense1 = localStorage.getItem('formData1');
+var data1 = [];
+
+if (table_depense1) {
+  data1 = JSON.parse(table_depense1);
+}
+
+afficherTableau1(data1);
